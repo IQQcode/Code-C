1a1386cffb324c8f4a5f6deb4eea422edb6859b3
Mr.Q <2726109782@qq.com>
2018-11-06 22:04:15 +0800

    提及搜代码

--- Test 11-03/Test 11-03/11-03.c
+++ Test 11-03/Test 11-03/11-03.c
@@ -1,52 +1,94 @@
-//加法函数
-//#define _CRT_SECURE_NO_WARNINGS 1 
+#define _CRT_SECURE_NO_WARNINGS 1 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-int Add(int x, int y)
+void test()
 {
-	int z = x + y;
-	return z;
+	static int n = 0;                //int n = 0;
+	n++;
+	printf("%3d", n);
 }
-
 int main()
 {
+	int i = 0;
+	for(i=0; i<10; i++)
+	{
+		test();
 
-	int num1 = 0;
-	int num2 = 0;
-	int sum = 0;
-	printf("输入俩个操作数：");
-	scanf("%d %d", &num1, &num2);
-	sum = Add(num1, num2);
-	printf("sum=%d", sum);
-
+	}
 	system("pause");
 	return 0;
 }
 
 
 
+//int main()
+//	{
+//	int i = 0;
+//
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 9, 10 };
+//	for (i = 0; i <= sizeof(arr) / sizeof(arr[0]); i++)
+//	{
+//		arr[i]=0;
+//	}
+//
+//	for (i = 0; i <= sizeof(arr) / sizeof(arr[0]); i++)
+//	{
+//		printf("%2d",arr[i]);
+//	}
+//	printf("\n");
+//
+//	system("pause");
+//	return 0;
+//	}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1 ,2 ,3 ,4 ,5 ,6, 7, 8, 9, 10 };
+//	for (i = 0; i < 10; i++)
+//		for (i = 0; i <sizeof(arr)/sizeof(arr[0]); i++)      //for (i = 0; i < 10; i++)
+//	{
+//		printf("%3d", arr[i]);
+//	}
+//	printf("\n");
+//
+//  system("pause");
+//   return 0;
+//}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+////加法函数
+//#define _CRT_SECURE_NO_WARNINGS 1 
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	printf("输入俩个操作数：");
+//	scanf("%d %d", &num1, &num2);
+//	sum = Add(num1, num2);
+//	printf("sum=%d", sum);
+//
+//	system("pause");
+//	return 0;
+//}
+//
 
 
 
---
+++ 作业二/作业二/11-6.c
@@ -0,0 +1,185 @@
+#define _CRT_SECURE_NO_WARNINGS 1	
+#include<stdio.h>
+#include<stdlib.h>
+//求俩个数的最大公约数、最小公倍数：
+//更相减损术
+int main()
+{
+	int n, a, b, c;
+	n = 1;
+	printf("请输入两个整数 :");
+	scanf("%d %d", &a, &b);
+	if (a < b)
+	{
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+	}
+	while (n != 0)
+	{
+		a = a - b;
+		if (a < b)
+		{
+			a = a ^ b;    //异或操作，交换俩个数值
+			b = a ^ b;
+			a = a ^ b;
+		}
+		if (a == b)
+		{
+			n = 0;
+		}
+	}
+	printf("\n最大公约数是:%d\n", b);
+	system("pause");
+}
+
+
+
+//int main()
+//{
+//	int m= 0;
+//	int n = 0;
+//	int r = 0;
+//	int num1 = 0, num2 = 0, dou = 0;
+//	printf("请输入要求最大公因数的俩个数值：\n");
+//	scanf("%d %d", &m, &n);
+//
+//	num1 = m;
+//	num2 = n;
+//
+//	while( n % m )
+//	{
+//		r = n % m;
+//		n = m;
+//		m = r;
+//	}
+//	printf("俩数的最大公因数是：%d\n", m);
+//
+//	dou = (num1*num2) / m;
+//	printf("俩个数的最小公倍数是：%d\n", dou);
+//
+//	system("pause");
+//    return 0;
+//
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 0, b = 0, c = 0, d = 0;
+//	int dou = 0;
+//	int num1 = 0, num2 = 0;
+//	printf("请输入要求最大公因数的俩个数值：\n");
+//	scanf("%d %d", &a, &b);
+//	num1 = a;
+//	num2 = b;
+//	if (b > a)
+//	{
+//		//num1 = a;     //为什么会出问题？？？
+//		//num2 = b;
+//
+//		c = b;
+//		b = a;
+//		a = c;
+//	}
+//	d = a % b;
+//	while (d != 0)
+//	{
+//		a = b;
+//		b = d;
+//		d = a % b;
+//	}
+//	printf("俩数个的最大公因数是：%d\n", b);
+//
+//	dou = (num1*num2) / b;
+//	printf("俩个数的最小公倍数是：%d\n", dou);
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//三个数从大到小排序
+/*int main()
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int tmp = 0;
+	scanf("%d %d %d", &i, &j, &k);
+	printf("Enter the number:%d %d %d\n", i, j, k);
+
+	if (i > j)
+	{
+		tmp = i;
+		i = j;
+		j = tmp;
+	}
+	if (i > k)
+	{
+		tmp = i;
+		i = k;
+		k = tmp;
+	}
+	if (j> k)
+	{
+		tmp = j;
+		j = k;
+		k = tmp;
+	}
+	printf("%d > %d > %d", k, j, i);
+	system("pause");
+	return 0;
+}*/
+
+
+//求十个数中最大的数：
+/*int main()
+{
+	int i = 0;
+	int max = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	max = arr[0];
+	for (i = 0; i <= sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		if (arr[i] > max)
+			max = arr[i];
+	}
+	printf("max=%d\n", max);
+
+	system("pause");
+	return 0;
+}*/
+
+
+
+//交换俩个数：
+/*int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int exc = 0;
+	printf("Please enter the number:");
+	scanf("%d %d", &num1, &num2);
+
+	//exc=num1;
+	//num1 = num2;
+	//num2 = exc;
+
+	//num1 = num1 + num2;
+	//num2 = num1 - num2;
+	//num1 = num1 - num2;
+
+	//num1 = num1 ^ num2;
+	//num2 = num1 ^ num2;
+	//num1 = num1 ^ num2;
+
+ printf("num1=%d,num2=%d", num1, num2);
+
+	system("pause");
+	return 0;
+
+}*/
