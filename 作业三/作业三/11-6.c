b8f8577e489437ee55ece0d54849dd9c2bc1b44c
Mr.Q <2726109782@qq.com>
2018-11-09 07:53:24 +0800

    提交代码

--- 作业三/作业三/11-6.c
+++ 作业三/作业三/11-6.c
@@ -2,65 +2,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//计算：1/1 - 1/2 + 1/3 -1/4......+1/99 - 1/100
+
+
+
 //交换俩个数组的内容：
 int main()
 {
-	int i = 0, j = 0;
-	int arr1[] = {1,2,3,4,5,6,7,8,9,10};
-	int arr2[] = {0};
+	int i = 0;
+	int temp = 0;
+	int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
+	int arr2[10] = {0};
 
-	for (i = 0; i <= sizeof(arr1) / sizeof(arr1[0]); i++)
+	for (i = 0; i<10; i++)
 	{
-		for (j = 0; j <= sizeof(arr2) / sizeof(arr2[0]); j++)
-		{
-			arr1[i] = arr2[j];
-			printf("%s", arr1);
-		}
+		temp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = temp;
+	}
+	printf("arr1=");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+	printf("arr2=");
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr2[i]);
 	}
 	system("pause");
 	return 0;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //统计1到100中9出现的个数：
 //代码分析：9，90，99可以整除9；29-89的个位为9
-/*int main()
-{
-	int i = 0;
-	int count = 0;
-		for(i = 1; i <= 100; i++)
-		{
-			if (i % 10 == 9)
-			{
-				count++;
-				printf("%3d", i);
-			}
-			
-			if (i / 10 == 9)
-			{
-				count++;
-				printf("%3d", i);
-			}
-
-		}
-		printf("\n");
-	printf("count=%d", count-1);
-
-	system("pause");
-	return 0;
-}*/
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//		for(i = 1; i <= 100; i++)
+//		{
+//			if (i % 10 == 9)
+//			{
+//				count++;
+//				printf("%3d", i);
+//			}
+//			
+//			if (i / 10 == 9)
+//			{
+//				count++;
+//				printf("%3d", i);
+//			}
+//
+//		}
+//		printf("\n");
+//	printf("count=%d", count);
+//
+//	system("pause");
+//	return 0;
+//}
