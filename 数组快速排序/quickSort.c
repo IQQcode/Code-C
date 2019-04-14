7e3852b1dee8bf1548dd315fa5840e5a07db47ef
IQQcode <2726109782@qq.com>
2019-04-14 09:03:22 +0800

    Incomplete ChessBoard problem

--- 数组快速排序/quickSort.c
+++ 数组快速排序/quickSort.c
@@ -2,27 +2,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int a[101], n;
+//定义全局变量，这两个变量需要在子函数中使用
+int arr[100] = { 0 };
+int n = 0;
 
-void quicksort(int left, int right) {
-	int i = left, j = right, base = a[left], temp;//base中存的就是基准数
-	if (left > right) return;
-	while (i < j) {
-		while (a[j] >= base && i < j) j--;
+void quicksort(int left, int right) 
+{
+	int  tmp = 0;
+	if (left > right)
+		return 0;
+	int temp = arr[left]; //temp中存的就是基准数
+	int i = left;
+	int j = right;
+	while (i != j) 
+	{ 
+		//顺序很重要，j要先从右边开始找
+		while (arr[j] >= temp && i < j)
+			j--;
+		//i再从左边开始找
+		while (arr[i] <= temp && i < j)
+			i++;
 
-		while (a[i] <= base && i < j) i++;
-
-		if (i < j) { //交换两个数在数组中的位置
-			temp = a[i]; a[i] = a[j]; a[j] = temp;
+		if (i < j)//交换两个数在数组中的位置
+		{
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
 		}
 	}
 	//最终将基准数归位
-	a[left] = a[j];
-	a[j] = base;
-
-	quicksort(left, i - 1);//递归处理左边
-	quicksort(i + 1, right);//递归处理右边
+	arr[left] = arr[i];
+	arr[i] = temp;
+	quicksort(left, i - 1);//继续处理左边的，这里是一个递归的过程
+	quicksort(i + 1, right);//继续处理右边的 ，这里是一个递归的过程
 }
+
 
 int main()
 {
@@ -31,14 +45,14 @@ int main()
 	printf("请输入数组元素: ");
 	for (int i = 0; i < n; i++)
 	{
-		scanf("%d", &a[i]);
+		scanf("%d", &arr[i]);
 	}
 	quicksort(0, n - 1); //快速排序调用
 
 	printf("排序结果为 ");
 	for (int i = 0; i < n; i++)
 	{
-		printf("%d ", a[i]);
+		printf("%d ", arr[i]);
 	}
 	system("pause");
 	return 0;
